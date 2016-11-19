bb6d4996563d1ea32492af320e99505eca5f53c0
kischiman <kischi1@online.de>
2016-11-19 18:00:46 +0100

    added project

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PUNTutorial
+{
+    public class GameManager : Photon.PunBehaviour
+    {
+        public static GameManager instance;
+        public static GameObject localPlayer;
+
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings("PuzzleMaze");
+        }
+
+        void Awake()
+        {
+                if (instance != null)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+            PhotonNetwork.automaticallySyncScene = true;
+        }
+
+        public void JoinGame()
+        {
+                RoomOptions ro = new RoomOptions();
+                PhotonNetwork.JoinOrCreateRoom("Default Room", ro, null);
+
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined Room");
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                PhotonNetwork.LoadLevel("Level");
+            }
+        }
+
+        void OnLevelWasLoaded(int levelNumber)
+        {
+            if (!PhotonNetwork.inRoom) return;
+
+            localPlayer = PhotonNetwork.Instantiate(
+                "Player",
+                new Vector3(0, 0.5f, 0),
+                Quaternion.identity, 0);
+        }
+
+
+    }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PUNTutorial
+{
+    public class Player : Photon.PunBehaviour
+    {
+        Camera playerCam;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            playerCam = GetComponentInChildren<Camera>();
+
+            if (!photonView.isMine)
+            {
+                playerCam.gameObject.SetActive(false);
+            }
+        }
+    }
+}
